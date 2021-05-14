4c62066234890c9b5a711a6a28f639b56c3429a7
kingfaize <31122819+kingfaize@users.noreply.github.com>
2021-05-14 06:28:20 +0000

    Add files via upload

---
+++ Alphs Journey.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Alphs Journey.
+
+#A pograme detailing the generational upgraades of my first computer.
+
+#Global variables
+computerName = "My name is Alpha"
+myExistence = "I am the first computer Lord Faisal ever assembled. I was his first love, you know:>"
+invitationType ="I have evolved over he years and I wish to share my story"
+Year = 1
+
+parts1 = [cpu[Pentium 1 166mhz], psu[ 200watts psu]] 
+parts20 = [cpu[Pentium 1 200mhz], psu[ 200watts psu]] 
+
+
+
+print (myExistence)
+
+print (computerName)
+
+print (invitationType)
+
+
+# In[ ]:
+
+
+# if Year = 1:
+    
+    parts = parts1
+  
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
